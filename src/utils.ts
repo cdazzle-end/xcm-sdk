fe006030a88bdc158930c56cb0122a94bb4893e6
dazzlec123 <dazzlec123@gmail.com>
2024-09-22 14:50:55 +0000

    Sep 22, 2024, 10:50 AM

--- src/utils.ts
+++ src/utils.ts
@@ -24,10 +24,10 @@ export const createAccID = (api: ApiPromise, account: string): HexString => {
 
 export const getFees = (scenario: TScenario): number => {
   if (scenario === 'ParaToRelay') {
-    console.log('Asigning fees for transfer to Relay chain')
+    // console.log('Asigning fees for transfer to Relay chain')
     return 4600000000
   } else if (scenario === 'ParaToPara') {
-    console.log('Asigning fees for transfer to another Parachain chain')
+    // console.log('Asigning fees for transfer to another Parachain chain')
     return 399600000000
   }
   throw new Error(`Fees for scenario ${scenario} are not defined.`)
